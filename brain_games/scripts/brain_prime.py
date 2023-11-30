7a0cc611d7e81c7a742d8020c120162003b84055
anns7one <obydennyhanna@gmail.com>
2023-11-30 10:04:25 +0200

    add brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,4 +1,12 @@
 from brain_games.cli import welcome_user
+import random
+
+
+def is_prime(num):
+    for x in range(2, num):
+        if num % x == 0:
+            return False
+    return True
 
 
 def main():
@@ -7,14 +15,17 @@ def main():
     answers_count = 0
     correct_answers_count = 0
     while answers_count < 3:
-        
-        user_answer = int(input("Your answer: "))
-        if user_answer == int(correct_result):
+        num = random.randint(1, 3571)
+        print(f"Question: {num}")
+        user_answer = input("Your answer: ")
+        if (user_answer == 'yes' and is_prime(num)) or \
+           (user_answer == 'no' and not is_prime(num)):
             print("Correct!")
             correct_answers_count += 1
         else:
-            print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_result}'.")
+            correct_answer = 'yes' if is_prime(num) else 'no'
+            print(f"'{user_answer}' is wrong answer ;(."
+                  f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again! {user}")
         answers_count += 1
     if correct_answers_count == 3:
